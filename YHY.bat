f9f199bb2a6900489d1c234db15bced948333333
leo-liu-exe <59662652+leo-liu-exe@users.noreply.github.com>
2020-10-29 14:56:45 -0700

    Add files via upload

---
+++ YHY.bat
@@ -0,0 +1,26 @@
+color 1a
+echo YHYHYHYHYHY
+color 2b
+echo YHYHYHYHYHY
+color 3c
+echo YHYHYHYHYHY
+color 4d
+echo YHYHYHYHYHY
+color 5e
+echo YHYHYHYHYHY
+color 6f
+echo YHYHYHYHYHY
+color 1a
+echo YHYHYHYHYHY
+color 2b
+echo YHYHYHYHYHY
+color 3c
+echo YHYHYHYHYHY
+color 4d
+echo YHYHYHYHYHY
+color 5e
+echo YHYHYHYHYHY
+color 6f
+echo YHYHYHYHYHY
+echo
+%0
